3a126e63d88239eb25abd9edf8b2478deb11f35c
Gibat <vgibat@gmail.com>
2013-04-13 00:28:19 +0400

    обновление
    добавил ссылки на админки в админке :D
    жду багрепортов и\или дизайна для дальнейших правок

--- protected/views/admin/index.php
+++ protected/views/admin/index.php
@@ -6,6 +6,12 @@ $this->pageTitle=Yii::app()->name;
 <br />
 <a href="<?php echo $this->createAbsoluteUrl('users/admin'); ?>">Управление пользователями</a>
 <br />
+<a href="<?php echo $this->createAbsoluteUrl('gallery/admin'); ?>">Управление альбомами</a>
+<br />
+<a href="<?php echo $this->createAbsoluteUrl('profile/admin'); ?>">Управление профилями пользователей</a>
+<br />
+<a href="<?php echo $this->createAbsoluteUrl('photo/admin'); ?>">Управление фотками</a>
+<br />
 <a href="<?php echo $this->createAbsoluteUrl('/rights'); ?>">Управление правами (rights)</a>
 <br />
 Управление форумом осуществляется в самом форуме
